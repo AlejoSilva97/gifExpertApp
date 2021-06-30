d9aedcf0c9ccadd6190538adc161774de2c42bb5
Alejandro Silva <alejandrosilva1897@gmail.com>
2021-06-30 12:12:53 -0500

    Comentarios agregados en el componente GifGrid.js

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -4,12 +4,15 @@ import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
+    //Este es un custom hook creado por nosotros mismos
     const {data:images, loading} = useFetchGifs(category);
 
     return (
         <>
+            {/* Estas clases son animaciones de la libreria animate.css */}
             <h3 className="animate__animated animate__fadeIn">{category}</h3>
 
+            {/* Si loading esta true entonces ponemos un parrafo que indica que esta cargando las imagenes */}
             {loading && <p className="animate__animated animate__flash">Loading...</p>}
 
             <div className="card-grid">
